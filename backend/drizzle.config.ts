e3d299c1a70bef13b2c011aa0df689a05a2cca23
Tiago Dinis <tiagodinis33@proton.me>
2026-05-13 21:09:20 +0100

    fix: drizzle-kit config and scripts

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,12 +1,12 @@
-import { Drizzle } from "$services"
 import { defineConfig } from "drizzle-kit"
 import { Effect, pipe } from "effect"
+import { Drizzle } from "./services"
 
 export default pipe(
 	Drizzle.Config.use((config) =>
 		defineConfig({
 			dialect: "postgresql",
-			schema: "./services/Drizzle/schema",
+			schema: "./backend/services/Drizzle/schema/",
 			dbCredentials: {
 				url: String(config.databaseUrl),
 			},
