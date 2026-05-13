abb7782f822fe6e4a872c4a092c365deb39ed6a3
Tiago Dinis <tiagodinis33@proton.me>
2026-05-13 21:19:31 +0100

    fix: switch from assets.d.ts to using `next typegen` command

--- radiant-frontend/types/assets.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.png" {
-	const content: import("next/image").StaticImageData
-	export default content
-}
-
-declare module "*.svg" {
-	const content: import("next/image").StaticImageData
-	export default content
-}
